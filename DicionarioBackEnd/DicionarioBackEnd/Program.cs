464eeee807c24763dffa3898972cfc29fe23a24d
Richardson <richardm@ucl.br>
2022-10-23 15:45:44 -0300

    Initial commit

---
+++ DicionarioBackEnd/DicionarioBackEnd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DicionarioBackEnd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
